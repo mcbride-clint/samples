cf5b9787e702f6e47dbd85a9c71d6d43903f1d88
mcbride-clint <cmm5559@gmail.com>
2020-03-17 07:04:43 -0400

    Restructure to clarify intent of UI Projects

--- Simple Architecture/SimpleArchitecture/SimpleArchitecture.RazorPages/Pages/Index.cshtml.cs
+++ Simple Architecture/SimpleArchitecture/SimpleArchitecture.RazorPages/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace SimpleArchitecture.Pages
+namespace SimpleArchitecture.RazorPages.Pages
 {
     public class IndexModel : PageModel
     {
--- Simple Architecture/SimpleArchitecture/SimpleArchitecture.RazorPages/Program.cs
+++ Simple Architecture/SimpleArchitecture/SimpleArchitecture.RazorPages/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace SimpleArchitecture
+namespace SimpleArchitecture.RazorPages
 {
     public class Program
     {
