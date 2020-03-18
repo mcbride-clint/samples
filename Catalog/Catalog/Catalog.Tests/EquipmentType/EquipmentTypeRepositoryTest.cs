0a14910d7284b53b5b76a8620985dfd67f34158e
mcbride-clint <cmm5559@gmail.com>
2020-03-18 15:54:55 -0400

    Additional Equipment Type Repo Tests

--- Catalog/Catalog/Catalog.Tests/EquipmentType/EquipmentTypeRepositoryTest.cs
+++ Catalog/Catalog/Catalog.Tests/EquipmentType/EquipmentTypeRepositoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using Catalog.DataAccess.Repositories;
+using Catalog.Models.DomainModels;
 using Catalog.Models.Filters;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -11,13 +12,75 @@ namespace Catalog.Tests.Designator
     [TestClass]
     public class EquipmentTypeRepositoryTest
     {
-        [TestMethod]
-        public void GetAllEquipmentTypes() {
-            var repo = new EquipmentTypeRepositoryInMemory();
+        private EquipmentType testingEquipmentType { get; set; }
+        private EquipmentTypeRepositoryInMemory repo { get; set; }
 
+        public EquipmentTypeRepositoryTest()
+        {
+            repo = new EquipmentTypeRepositoryInMemory();
+
+            testingEquipmentType = new EquipmentType()
+            {
+                Name = "Quick Test",
+                Description = "Will be used for testing mainpulation of Repo"
+            };
+        }
+
+
+        [TestMethod]
+        public void GetAllEquipmentTypes()
+        {
             var items = repo.Find();
 
             Assert.IsTrue(items.Any());
+        }
+
+        [TestMethod]
+        public void InsertEquipmentTypeToRepo()
+        {
+            var insertedType = repo.Insert(testingEquipmentType);
+
+            var foundType = repo.Find(insertedType.EquipmentTypeId);
+
+            Assert.AreEqual(insertedType, foundType);
+        }
+
+        [TestMethod]
+        public void UpdateEquipmentTypeToRepo()
+        {
+            var insertedType = repo.Insert(testingEquipmentType);
+            insertedType = repo.Find(insertedType.EquipmentTypeId);
+
+            var updatingType = new EquipmentType()
+            {
+                EquipmentTypeId = insertedType.EquipmentTypeId,
+                Name = "Updated Name",
+                Description = "Updated Description"
+            };
+
+            repo.Update(updatingType);
+
+            var foundType = repo.Find(insertedType.EquipmentTypeId);
+
+            Assert.AreEqual(updatingType, foundType);
+            Assert.AreEqual(updatingType.EquipmentTypeId, foundType.EquipmentTypeId);
+            Assert.AreEqual(updatingType.Name, foundType.Name);
+            Assert.AreEqual(updatingType.Description, foundType.Description);
+
+            Assert.AreEqual(foundType.EquipmentTypeId, insertedType.EquipmentTypeId);
+            Assert.AreNotEqual(foundType.Name, insertedType.Name);
+            Assert.AreNotEqual(foundType.Description, insertedType.Description);
+        }
+
+        [TestMethod]
+        public void DeleteEquipmentTypeFromRepo()
+        {
+            var insertedType = repo.Insert(testingEquipmentType);
+            insertedType = repo.Find(insertedType.EquipmentTypeId);
+
+            repo.Delete(insertedType);
+
+            Assert.ThrowsException<InvalidOperationException>(() => repo.Find(insertedType.EquipmentTypeId));
         }
     }
 }
