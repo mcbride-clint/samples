cc3dd4f118d69be03d16e193f49a9d5de86ce3c1
mcbride-clint <cmm5559@gmail.com>
2020-03-17 22:18:48 -0400

    Create MVC Variant of Application

---
+++ Simple Architecture/SimpleArchitecture/SimpleArchitecture.Mvc/SimpleArchitecture.Mvc/Controllers/UsersController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SimpleArchitecture.Models.DomainModels;
+using SimpleArchitecture.Models.Filters;
+using SimpleArchitecture.Services;
+
+namespace SimpleArchitecture.Mvc.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ILogger<UsersController> _logger;
+        private readonly UserService _userService;
+
+        public UsersController(ILogger<UsersController> logger, UserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var users = _userService.Find(new UserFilter() );
+            return View(users);
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int id)
+        {
+            var user = _userService.Find(id);
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(User entity)
+        {
+            try
+            {
+                _userService.Save(entity);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("Error", ex.Message);
+                return View(entity);
+            }
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var user = _userService.Find(id);
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, User entity)
+        {
+            try
+            {
+                _userService.Save(entity);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Error", ex.Message);
+                return View(entity);
+            }
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var user = _userService.Find(id);
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, User entity)
+        {
+            try
+            {
+                entity = _userService.Find(id);
+                _userService.Delete(entity);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Error", ex.Message);
+                return View(entity);
+            }
+        }
+    }
+}
---
+++ Simple Architecture/SimpleArchitecture/SimpleArchitecture.Mvc/SimpleArchitecture.Mvc/Startup.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SimpleArchitecture.DataAccess.Repositories;
+using SimpleArchitecture.Models.Interfaces.Repositories;
+using SimpleArchitecture.Services;
+
+namespace SimpleArchitecture.Mvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // ****************************8
+            // Begin Custom Setup
+            // ****************************8
+
+            // Inject the UserRepositoryInMemory into needed IUserRepository Instances. 
+            // This Instance is a Singleton so there will ever only be 1 between HTTP Transactions to Store Users in memory
+            services.AddSingleton<IUserRepository, UserRepositoryInMemory>();
+
+            // Inject a User Service Wherever Needed
+            services.AddScoped<UserService>();
+
+            // There is a Default ILogger Injected that will log messages to the Output and Debug Visual Studio Windows at Information and above levels
+            // Inject a different one to redirect to other destinations
+
+            // ****************************8
+            // End Custom Setup
+            // ****************************8
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
