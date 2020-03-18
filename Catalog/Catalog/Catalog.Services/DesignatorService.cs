55f5145502a4f7f38fb317108caba33e09527b74
mcbride-clint <cmm5559@gmail.com>
2020-03-18 11:53:16 -0400

    Designator Service Creation and Testing

---
+++ Catalog/Catalog/Catalog.Services/DesignatorService.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Models.DomainModels;
+using Catalog.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Services
+{
+    public class DesignatorService
+    {
+        private readonly IDesignatorRepository _designatorRepo;
+
+        public DesignatorService(IDesignatorRepository designatorRepo)
+        {
+            _designatorRepo = designatorRepo;
+        }
+
+        public IEnumerable<Designator> GetAll()
+        {
+            return _designatorRepo.Find(new Models.Filters.DesignatorFilter());
+        }
+    }
+}
---
+++ Catalog/Catalog/Catalog.Tests/Designator/DesignatorServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.DataAccess.Repositories;
+using Catalog.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Catalog.Tests.Designator
+{
+    [TestClass]
+    public class DesignatorServiceTest
+    {
+        private readonly DesignatorService _designatorService;
+
+        public DesignatorServiceTest()
+        {
+            var designatorRepo = new DesignatorRepositoryInMemory();
+            _designatorService = new DesignatorService(designatorRepo);
+        }
+        [TestMethod]
+        public void GetAllDesignators()
+        {
+            var items = _designatorService.GetAll();
+
+            Assert.IsTrue(items.Any());
+        }
+    }
+}
