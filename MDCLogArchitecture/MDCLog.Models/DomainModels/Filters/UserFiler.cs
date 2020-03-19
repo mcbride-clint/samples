01b3857dc43e5d2b31f12acae7c58b7f9e2d495e
chasjefferson <chasjefferson@msn.com>
2020-03-19 13:53:55 -0400

    wi1886 added user filters and IlogComments interface

---
+++ MDCLogArchitecture/MDCLog.Models/DomainModels/Filters/UserFiler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDCLog.Models.DomainModels.Filters
+{
+    public class UserFilter
+    {
+    }
+}
---
+++ MDCLogArchitecture/MDCLog.Models/DomainModels/Interfaces/Repositories/Interface1.cs
@@ -0,0 +1,17 @@
+﻿using MDCLog.Models.DomainModels.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDCLog.Models.DomainModels.Interfaces.Repositories
+{
+    interface ILogComments
+    {
+        LogComments Insert(LogComments entity);
+        LogComments Update(LogComments entity);
+        void Delete(LogComments entity);
+        LogComments Find(int LogNumber);
+        List<LogComments> Find(UserFilter filter);
+
+    }
+}
