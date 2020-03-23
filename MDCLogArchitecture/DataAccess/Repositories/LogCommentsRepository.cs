86395e680152218515af26c737a574ccbbdfb054
chasjefferson <chasjefferson@msn.com>
2020-03-23 10:03:51 -0400

    wi 1886 Reorganized the code structure - I belive I am on the right oath now

--- MDCLogArchitecture/DataAccess/Repositories/LogCommentsRepository.cs
+++ MDCLogArchitecture/DataAccess/Repositories/LogCommentsRepository.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using MDCLogArchitecture.Models.DomainModels;
+using MDCLogArchitecture.Models.Filters;
+using MDCLogArchitecture.Models.Interfaces.Repositories;
 
 
-namespace DataAccess.Repositories
+namespace MDCLogArchitecture.DataAccess.Repositories
 {
-    class LogCommentsRepository 
+    class LogCommentsRepository : ILogCommentsRepository
     {
+        public void Delete(LogComments entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogComments Find(int SeqNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<LogComments> Find(LogCommentsFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogComments Insert(LogComments entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- MDCLogArchitecture/MDCLog.Models/Filters/LogCommentsFilter.cs
+++ MDCLogArchitecture/MDCLog.Models/Filters/LogCommentsFilter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace MDCLogArchitecture.Models.DomainModels.Filters
+namespace MDCLogArchitecture.Models.Filters
 {
     public class LogCommentsFilter
     {
