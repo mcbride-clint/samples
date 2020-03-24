64ce1b293cc5329dfa0a25efb050fb6d93fe339a
mcbride-clint <cmm5559@gmail.com>
2020-03-24 12:50:38 -0400

    Create Basix Blazor Server Application with MatBlazor

---
+++ Simple Architecture/SimpleArchitecture/SimpleArchitechture.Blazor/SimpleArchitechture.Blazor/Startup.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MatBlazor;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SimpleArchitecture.DataAccess.Repositories;
+using SimpleArchitecture.Models.Interfaces.Repositories;
+using SimpleArchitecture.Services;
+
+namespace SimpleArchitecture.Blazor
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // ****************************8
+            // Begin Custom Setup
+            // ****************************8
+
+            // Inject the UserRepositoryInMemory into needed IUserRepository Instances. 
+            // This Instance is a Singleton so there will ever only be 1 between HTTP Transactions to Store Users in memory
+            services.AddSingleton<IUserRepository, UserRepositoryInMemory>();
+
+            // Inject a User Service Wherever Needed
+            services.AddScoped<UserService>();
+
+            // There is a Default ILogger Injected that will log messages to the Output and Debug Visual Studio Windows at Information and above levels
+            // Inject a different one to redirect to other destinations
+
+            services.AddScoped<HttpClient>();
+
+            services.AddMatToaster(config =>
+            {
+                config.Position = MatToastPosition.BottomRight;
+                config.PreventDuplicates = true;
+                config.NewestOnTop = true;
+                config.ShowCloseButton = true;
+                config.MaximumOpacity = 95;
+                config.VisibleStateDuration = 3000;
+            });
+
+            // ****************************8
+            // End Custom Setup
+            // ****************************8
+
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
---
+++ Simple Architecture/SimpleArchitecture/SimpleArchitechture.Blazor/SimpleArchitechture.Blazor/ViewModel/UserVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleArchitecture.Blazor.ViewModel
+{
+    public class UserVM
+    {
+        public long UserIdSeqNum { get; set; }
+        public string UserId { get; set; }
+        public string FullName { get; set; }
+        public string Activity { get; set; }
+        public string Subactivity { get; set; }
+        public string Owner { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
