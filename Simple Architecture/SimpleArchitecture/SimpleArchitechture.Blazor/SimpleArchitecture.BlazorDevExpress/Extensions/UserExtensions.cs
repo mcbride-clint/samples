68e6636d458718ac39683045e84eb668da8d6e0a
mcbride-clint <cmm5559@gmail.com>
2020-04-01 19:15:45 -0400

    Try out Dev Express Comonent Library

---
+++ Simple Architecture/SimpleArchitecture/SimpleArchitechture.Blazor/SimpleArchitecture.BlazorDevExpress/Extensions/UserExtensions.cs
@@ -0,0 +1,46 @@
+﻿using SimpleArchitecture.BlazorDevExpress.ViewModel;
+using SimpleArchitecture.Models.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleArchitecture.BlazorDevExpress.Extensions
+{
+    public static class UserExtensions
+    {
+        public static UserVM ToViewModel(this User user)
+        {
+            return new UserVM()
+            {
+                Activity = user.Activity,
+                FullName = user.FullName,
+                Owner = user.Owner,
+                PhoneNumber = user.PhoneNumber,
+                Subactivity = user.Subactivity,
+                UserId = user.UserId,
+                UserIdSeqNum = user.UserIdSeqNum
+            };
+        }
+
+        public static User ToDomainModel(this UserVM userVM)
+        {
+            return new User()
+            {
+                Activity = userVM.Activity,
+                FullName = userVM.FullName,
+                Owner = userVM.Owner,
+                PhoneNumber = userVM.PhoneNumber,
+                Subactivity = userVM.Subactivity,
+                UserId = userVM.UserId,
+                UserIdSeqNum = userVM.UserIdSeqNum
+            };
+        }
+
+
+        public static IEnumerable<UserVM> ToViewModel(this IEnumerable<User> users)
+        {
+            return users.Select(u => u.ToViewModel());
+        }
+    }
+}
---
+++ Simple Architecture/SimpleArchitecture/SimpleArchitechture.Blazor/SimpleArchitecture.BlazorDevExpress/Pages/Users/View.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using SimpleArchitecture.BlazorDevExpress.Extensions;
+using SimpleArchitecture.BlazorDevExpress.ViewModel;
+using SimpleArchitecture.Services;
+
+namespace SimpleArchitecture.BlazorDevExpress.Pages.Users
+{
+    public class ViewBase : ComponentBase
+    {
+        [Inject] UserService UserService { get; set; }
+        [Inject] NavigationManager NavigationManager { get; set; }
+
+        [Parameter] public int UserIdSeqNum { get; set; }
+
+
+        public UserVM User;
+
+        public bool IsReadonly { get; set; }
+
+        protected override void OnInitialized()
+        {
+            IsReadonly = true;
+            User = UserService.Find(UserIdSeqNum).ToViewModel();
+        }
+
+        protected void ToggleEdit()
+        {
+            IsReadonly = !IsReadonly;
+        }
+
+        protected void Submit()
+        {
+            UserService.Save(User.ToDomainModel());
+            //Toaster.Add($"{User.UserId} Saved Successfully", MatToastType.Success, icon: "edit");
+            RefreshPage();
+        }
+
+        protected void RefreshPage()
+        {
+            OnInitialized();
+            this.StateHasChanged();
+        }
+    }
+}
