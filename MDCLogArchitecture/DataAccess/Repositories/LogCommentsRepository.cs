de25c8e2f2f3b6d961473b9abdfb2f462484c826
chasjefferson <chasjefferson@msn.com>
2020-04-03 10:08:22 -0400

    Added Insert new comment and some navigation buttons

--- MDCLogArchitecture/DataAccess/Repositories/LogCommentsRepository.cs
+++ MDCLogArchitecture/DataAccess/Repositories/LogCommentsRepository.cs
@@ -12,12 +12,15 @@ using System.Data;
 
 namespace MDCLogArchitecture.DataAccess.Repositories
 {
-   public class LogCommentsRepository : ILogCommentsRepository
-    { 
-        private string listSQL = "Select SEQ_NUM as SeqNum,LOG_NUM as LogNumber,TYPE as Type,COMMENT_TEXT as Comment from TM_DIST_CHG_LOG_COMMENTS";
-        
-        readonly System.Data.IDbConnection _db; 
-        public LogCommentsRepository(IDbConnection db) {
+    public class LogCommentsRepository : ILogCommentsRepository
+    {
+        private string listSQL = "Select SEQ_NUM as SeqNum,LOG_NUM as LogNumber,TYPE as Type,TYPE_DESC as TypeDesc,COMMENT_TEXT as Comment," +
+            "BASIC_NUMBER as BasicNumber,CREATED_DATE as CreateDate,CREATED_BY_SEQ_NUM as CreatedBySeqNum," +
+            " CREATED_BY as CreatedBy from TM_DIST_CHG_LOG_COMMENTS";
+
+        readonly System.Data.IDbConnection _db;
+        public LogCommentsRepository(IDbConnection db)
+        {
             _db = db;
         }
 
@@ -31,11 +34,22 @@ namespace MDCLogArchitecture.DataAccess.Repositories
         public List<LogComments> FindList(LogCommentsFilter filter)
         {
             string mySQL = listSQL + " where Log_number = " + filter.LogNumber;
-                List<LogComments> comments = _db.Query<LogComments>(listSQL).ToList();
-                return comments;  
+            List<LogComments> comments = _db.Query<LogComments>(listSQL).ToList();
+            return comments;
         }
 
         public LogComments Insert(LogComments entity)
+        {
+            string mySQL = "INSERT INTO [TM_DIST_CHG_LOG_COMMENTS] ([LOG_NUM],[BASIC_NUMBER]" +
+    ",[TYPE],[LINK_TO_SEQ_NUM],[CREATED_DATE],[CREATED_BY_SEQ_NUM],[COMMENT_TEXT]" +
+      " ,[CREATED_BY],[TYPE_DESC]) VALUES " +
+        "(" + entity.LogNumber + ",'"+ entity.BasicNumber + "','"  + entity.Type.ToString() + "',"
+         + entity.LinkToSeqNum + "," + entity.CreateDate.ToShortDateString() + "," + entity.CreatedBySeqNum + ",'"
+         + entity.Comment.ToString() + "','" + entity.CreatedBy.ToString() + "','" + entity.TypeDesc.ToString() + "')";
+           int rowsAffected = _db.Execute(mySQL);
+            return entity;
+        }
+        public LogComments Save(LogComments entity)
         {
             throw new NotImplementedException();
         }
@@ -43,6 +57,6 @@ namespace MDCLogArchitecture.DataAccess.Repositories
 
 
 
-    
-  
+
+
 }
