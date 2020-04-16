a09e8ae43d54506cedb5cd8569a7c3a66379e33e
mcbride-clint <cmm5559@gmail.com>
2020-04-16 14:42:59 -0400

    Fix Prioroty Code Interface and Exclude obj

--- MDCLogArchitecture/MDCLog.Models/DomainModels/PriorityCode.cs
+++ MDCLogArchitecture/MDCLog.Models/DomainModels/PriorityCode.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using MDCLogArchitecture.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MDCLogArchitecture.Models.DomainModels
 {
-    public class PriorityCode
+    public class PriorityCode : IPriorityCode
     {
         public string Priority { get; set; }
         public string Descr { get; set; }
