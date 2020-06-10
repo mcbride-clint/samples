e4946e935c6c8d140b4816279f4f10ecf719fddc
chasjefferson <chasjefferson@msn.com>
2020-06-10 09:37:25 -0400

    AddHandlerCodes - added application model do,ain models and Repository interface and service. Will add the infrastructure and ui pieces in the next commit

---
+++ MdcLog/MdcLog.Application/HandlerCodes/IHandlerCodeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using MdcLog.Domain.Entities;
+using System.Text;
+
+namespace MdcLog.Application.HandlerCodes
+{
+    public interface IHandlerCodeRepository
+    {
+        HandlerCode InsertHandlerCode(HandlerCode entity);
+        HandlerCode SaveType(HandlerCode entity);
+        HandlerCode EditHandlerCode(HandlerCode entity);
+        HandlerCode FindHandlerCode(string Code);
+        ICollection<HandlerCode> FindHandlerCodeList();
+        HandlerCode DeleteHandlerCode(HandlerCode entity);
+    }
+}
